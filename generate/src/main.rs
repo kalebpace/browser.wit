6102e41cfc5f1f705d1bb23435cf9d0288821f47
Kaleb Pace <kaleb.pace@pm.me>
2024-07-26 22:01:28 -0500

    fix(generate): panic Base dictionary not found

--- generate/src/main.rs
+++ generate/src/main.rs
@@ -1,17 +1,22 @@
-use std::fs;
+use std::{env, fs};
 
 use webidl2wit::PackageName;
-use weedle::Parse;
+use weedle::{Definition, Parse};
 
 fn main() {
-    let mut webidl_ast = vec![];
-    for path in fs::read_dir("../webidl").unwrap() {
-        let contents = fs::read_to_string(path.unwrap().path()).unwrap();
-        // TODO: find a better solution than leaking
-        let contents = Box::leak(Box::new(contents));
-        let mut webidl = weedle::Definitions::parse(contents).unwrap().1;
-        webidl_ast.append(&mut webidl)
-    }
+    let cwd = env::current_dir().expect("Could not get current working directory");
+    println!("Running from directory: {:?}", cwd);
+
+    let paths = fs::read_dir("../../webidl").expect("Failed to read dir");
+    let contents = paths
+        .into_iter()
+        .filter_map(|path| fs::read_to_string(path.unwrap().path()).ok())
+        .collect::<Vec<String>>();
+    let webidl_ast = contents
+        .iter()
+        .flat_map(|content| weedle::Definitions::parse(&content).unwrap().1)
+        .collect::<Vec<Definition>>();
+
     let wit_ast = webidl2wit::webidl_to_wit(
         webidl_ast,
         webidl2wit::ConversionOptions {
@@ -103,5 +108,5 @@ fn main() {
     )
     .unwrap();
     let wit_output = wit_ast.to_string();
-    std::fs::write("../wit/web.wit", wit_output).unwrap();
+    std::fs::write("../../wit/web.wit", wit_output).unwrap();
 }
